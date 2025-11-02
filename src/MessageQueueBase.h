37127c0c845c7670c965dc89e41706346bfb9bc0
Martin Budden <mjbudden@gmail.com>
2025-11-02 07:14:03 +0000

    Renamed WAIT_IF_EMPTY to WAIT

--- src/MessageQueueBase.h
+++ src/MessageQueueBase.h
@@ -6,5 +6,5 @@
 class MessageQueueBase {
 public:
     virtual ~MessageQueueBase() = default;
-    virtual int32_t WAIT_IF_EMPTY(uint32_t& timeMicroseconds) const = 0;
+    virtual int32_t WAIT(uint32_t& timeMicroseconds) const = 0;
 };
